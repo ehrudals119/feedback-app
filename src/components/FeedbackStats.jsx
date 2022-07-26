6ee17557ef679bbbee4bbc13dcb282dbbca7bc27
Kyungmin Do <tommydo58@gmail.com>
2022-07-26 01:26:09 -0400

    initial

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import {useContext} from 'react'
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackStats() {
+  const {feedback} = useContext(FeedbackContext)
+
+  // calculate ratings avg
+  let avg = feedback.reduce((acc, cur)=> {
+    return acc + cur.rating
+  }, 0) / feedback.length 
+
+  avg = avg.toFixed(1)
+
+  return (
+    <div className='feedback-stats'>
+        <h4>{feedback.length} Reviews</h4>
+        <h4>Average Rating: {isNaN(avg) ? 0 : avg}</h4>
+    </div>
+  )
+}
+
+export default FeedbackStats
